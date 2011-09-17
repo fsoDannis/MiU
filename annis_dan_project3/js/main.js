4ce529163fbddf6959640f73bda35fc130fd94f1
Dan <Dan@Dinkydetails.com>
2011-09-17 15:33:24 -0400

    Back on track

---
+++ annis_dan_project3/js/main.js
@@ -0,0 +1,19 @@
+/*var parserForm= function(data){
+	console.log(data);
+	
+};*/
+
+$(document).ready(function(){
+	
+	var rForm = $('#request');
+	
+	rForm.validate();
+	/*	invalidHandler: function(form, validator){},
+		submitHandler: function(){
+			var data = rForm.serializeArray();
+			parserForm(data);
+		
+		}	
+	}*/
+});
+	

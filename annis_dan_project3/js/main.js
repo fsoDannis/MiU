a0fdc721952ad93b7b08f2849cc6a94ca186fc6d
Dan <Dan@Dinkydetails.com>
2011-09-17 15:43:18 -0400

    made some changes

--- annis_dan_project3/js/main.js
+++ annis_dan_project3/js/main.js
@@ -1,19 +1,8 @@
-/*var parserForm= function(data){
-	console.log(data);
-	
-};*/
-
 $(document).ready(function(){
 	
 	var rForm = $('#request');
 	
 	rForm.validate();
-	/*	invalidHandler: function(form, validator){},
-		submitHandler: function(){
-			var data = rForm.serializeArray();
-			parserForm(data);
-		
-		}	
-	}*/
+
 });
 	

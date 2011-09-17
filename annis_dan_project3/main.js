e1ae6bfc0a2e5692aa94953f8b8bee1111919814
Dan <Dan@Dinkydetails.com>
2011-09-17 15:44:31 -0400

    Fixed main.js

--- annis_dan_project3/main.js
+++ annis_dan_project3/main.js
@@ -1,17 +1,8 @@
-var parserForm= function(data){
-	console.log(data);
-	
-};
-
 $(document).ready(function(){
 	
 	var rForm = $('#request');
 	
-	rForm.validate({
-		invalidHandler: function(form, validator){},
-		submitHandler: function(){
-			var data = rForm.serializeArray();
-			parserForm(data);
-		}
-	};	
+	rForm.validate();
+
 });
+	
